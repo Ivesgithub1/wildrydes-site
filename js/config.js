5836ae38d0411087ded0c3b35003a6a84efbd962
Ivesgithub1 <117032520+Ivesgithub1@users.noreply.github.com>
2024-12-29 08:54:07 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_mxxSZaq46', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '27uno5bu8630sfkb4s1717gd5d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_DSvxfsShw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'c0b87gocobct4voqpplngg0nh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
